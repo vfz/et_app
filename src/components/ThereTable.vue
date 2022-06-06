71bc7542a7da0eed7f45cf9c6916ae3162b1ba43
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-06 12:50:06 +0300

    dispatch modal created

--- src/components/ThereTable.vue
+++ src/components/ThereTable.vue
@@ -56,7 +56,7 @@
                   <div class="dispatch-city">
                     Ставрополь
                   </div>
-                  <div class="dispatch-place">
+                  <div class="dispatch-place" data-bs-toggle="modal" data-bs-target="#dispatch-modal">
                     ж/д вокзал
                   </div>
                 </td>
@@ -116,7 +116,25 @@
         </div>
       </div>
     </div>
+    <div class="modal fade" id="dispatch-modal" tabindex="-1" aria-labelledby="dispatch-modal-label" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
+  <!-- Modal -->
 </template>
 
 <script>

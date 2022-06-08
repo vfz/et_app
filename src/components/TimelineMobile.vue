b0a699c313704944b2b5dabb7370255d17a0e4f9
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-08 15:14:28 +0300

    added code to TimelineMobile.vue

--- src/components/TimelineMobile.vue
+++ src/components/TimelineMobile.vue
@@ -1,13 +1,116 @@
 <template>
-
+  <div>
+    <section class="timeline-section">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div v-if="currentRouteName === 'Flight-selection'" class="timeline-wrapper d-flex">
+              <div class="timeline-item active">
+                <h1 class="timeline-item-title">
+                  Выбор рейса
+                </h1>
+              </div>
+              <div class="timeline-item">
+                <h1 class="timeline-item-title">
+                  Бронирование билета
+                </h1>
+              </div>
+              <div class="timeline-item">
+                <h1 class="timeline-item-title">
+                  Оплата
+                </h1>
+              </div>
+            </div>
+            <div v-if="currentRouteName === 'Ticket-booking'" class="timeline-wrapper d-flex">
+              <div class="timeline-item ready">
+                <h1 class="timeline-item-title">
+                  Выбор рейса
+                </h1>
+              </div>
+              <div class="timeline-item active">
+                <h1 class="timeline-item-title">
+                  Бронирование билета
+                </h1>
+              </div>
+              <div class="timeline-item">
+                <h1 class="timeline-item-title">
+                  Оплата
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TimelineMobile"
+  name: "TimelineMobile",
+  computed: {
+    currentRouteName() {
+      return this.$route.name;
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.timeline-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  .active {
+    .timeline-item-title {
+      color: $black;
+    }
+    .timeline-item-underline {
+      background-color: $black;
+      .timeline-item-underline-point {
+        background-color: $black;
+      }
+    }
+    .timeline-item-description {
+      color: $black;
+    }
+  }
+  .ready {
+    .timeline-item-title {
+      color: $blue-active;
+    }
+    .timeline-item-underline {
+      background-color: $blue-active;
+      .timeline-item-underline-point {
+        background-color: $blue-active;
+      }
+    }
+    .timeline-item-description {
+      color: $blue-active;
+    }
+  }
+  .timeline-item {
+    &-title {
+      font-family: $uni;
+      font-weight: $bold;
+      font-size: 36px;
+      color: $deactivate;
+      padding-right: 40px;
+      @media screen and (max-width: 1011px) {
+        font-size: 30px;
+      }
+      @media screen and (max-width: 894px) {
+        font-size: 23px;
+      }
+      @media screen and (max-width: 768px) {
+        font-size: 20px;
+      }
+    }
+  }
+  @media screen and (max-width: 767px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+}
 </style>

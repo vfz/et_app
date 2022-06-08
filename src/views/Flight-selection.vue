fe65ee3c3ca2490d1be18b8d99b60e9b0db5b291
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-08 12:57:07 +0300

    submit-btn for Flight-selection.vue created

--- src/views/Flight-selection.vue
+++ src/views/Flight-selection.vue
@@ -5,6 +5,15 @@
     <FlightForm/>
     <ThereTable/>
     <BackTable/>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+          <button type="button" class="btn submit-flight btn-primary">
+            Перейти к бронированию
+          </button>
+        </div>
+      </div>
+    </div>
   <Footer/>
   <LoginModal/>
   <RegistrationModal/>
@@ -16,6 +25,16 @@
 @import "src/assets/font.scss";
 .body-app-white {
   background-color: $white;
+  .btn {
+    @include font($uni,$regular,16px,21.6px,$white);
+    padding: 11px 47px;
+    border-radius: 8px;
+    border: none;
+    color: $white;
+    background-color: $blue-active;
+    margin-bottom: 72px;
+    text-transform: none;
+  }
 }
 </style>
 <script>

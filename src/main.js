8c3569f88465c1922f57bad1d3c851461a89a861
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-10 17:02:09 +0300

    removed bootstrap min js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-// import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'

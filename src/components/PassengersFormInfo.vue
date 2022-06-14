be571a1507fa07acc31e480b167eeb0ec4ebad24
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 14:16:17 +0300

    title section styled

--- src/components/PassengersFormInfo.vue
+++ src/components/PassengersFormInfo.vue
@@ -22,5 +22,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.passengers-form-info-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  margin-top: 40px;
+  .title-section {
+    @include font($uni,$bold,24px,32.4px,$base);
+    margin-bottom: 16px;
+  }
+  .title-section-description {
+    @include font($uni,$light,18px,24.3px,$base);
+    margin-bottom: 0;
+  }
+}
 </style>

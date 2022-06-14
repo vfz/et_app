aecde305154a6df915bc89c44ef77fa604e9e66a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 10:05:56 +0300

    imported Timeline.vue and FlightForm.vue

--- src/views/Ticket-booking.vue
+++ src/views/Ticket-booking.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="body-app-white">
     <Header/>
+    <Timeline/>
+    <FlightForm/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Timeline from "@/components/desktop/Timeline";
+import FlightForm from "@/components/Flight-form";
+import Footer from "@/components/Footer";
 export default {
   name: "Ticket-booking",
-  components: {Header}
+  components: {Footer, FlightForm, Timeline, Header}
 }
 </script>
 

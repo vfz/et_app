7548ee43013044f2fc867a3c3cbcec3b27d7f7ff
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 14:12:41 +0300

    Created PassengersFormInfo.vue

---
+++ src/components/PassengersFormInfo.vue
@@ -0,0 +1,26 @@
+<template>
+<section class="passengers-form-info-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="title-section">
+          Данные пассажиров
+        </h2>
+        <p class="title-section-description">
+          Заполните ниже данные, для оформления билета
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: "PassengersFormInfo"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

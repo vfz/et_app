c5e26a0e24b41f9f4ef117f00233e8d3ace1a581
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 10:20:48 +0300

    added layout of tabs

--- src/components/ThereBackTabs.vue
+++ src/components/ThereBackTabs.vue
@@ -1,5 +1,24 @@
 <template>
-
+  <section class="there-back-tabs-section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+              <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+              <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+              <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+            </div>
+          </nav>
+          <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,6 +27,14 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.there-back-tabs-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  .nav-tabs {
+    .nav-link {
 
+    }
+  }
+}
 </style>

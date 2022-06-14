91e4b74129f5867fc00f2a2b022436b2cd3e945c
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 10:27:59 +0300

    added wrapper and styles for this

--- src/components/ThereBackTabs.vue
+++ src/components/ThereBackTabs.vue
@@ -3,15 +3,17 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <nav>
-            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-              <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Туда</button>
-              <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Обратно</button>
+          <div class="there-back-tabs-wrapper">
+            <nav>
+              <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Туда</button>
+                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Обратно</button>
+              </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+              <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+              <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
             </div>
-          </nav>
-          <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
           </div>
         </div>
       </div>
@@ -33,9 +35,17 @@ export default {
   margin-left: 88px;
   margin-right: 88px;
   margin-top: 40px;
-  .nav-tabs {
-    .nav-link {
-      @include font($uni,$bold,24px,32.4px,$base);
+  .there-back-tabs-wrapper {
+    .nav-tabs {
+      border-radius: 16px;
+      .nav-link {
+        @include font($uni,$bold,24px,32.4px,$base);
+      }
+      .active {
+        border-color: unset;
+        border-bottom: none;
+        box-shadow: $regular-shadow;
+      }
     }
   }
 }

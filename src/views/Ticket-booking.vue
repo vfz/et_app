a339f3a8894cfade0915846a6a3f0a0b098d326a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-14 10:08:31 +0300

    imported LoginModal,RegistrationModal and ForgotPasswordModal.vue

--- src/views/Ticket-booking.vue
+++ src/views/Ticket-booking.vue
@@ -4,6 +4,9 @@
     <Timeline/>
     <FlightForm/>
     <Footer/>
+    <LoginModal/>
+    <RegistrationModal/>
+    <ForgotPasswordModal/>
   </div>
 </template>
 
@@ -12,9 +15,12 @@ import Header from "@/components/Header";
 import Timeline from "@/components/desktop/Timeline";
 import FlightForm from "@/components/Flight-form";
 import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
   name: "Ticket-booking",
-  components: {Footer, FlightForm, Timeline, Header}
+  components: {Footer, FlightForm, Timeline, Header, LoginModal, RegistrationModal, ForgotPasswordModal}
 }
 </script>
 

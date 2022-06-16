95ee3d7dad28a893bb7f7fb3a9edb4f22d5bf7e9
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-16 15:42:30 +0300

    created layout and style for RemoveModal.vue

--- src/components/RemoveModal.vue
+++ src/components/RemoveModal.vue
@@ -1,18 +1,24 @@
 <template>
   <!-- Modal -->
   <div class="modal fade" id="removeModal" tabindex="-1" aria-labelledby="removeModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <p class="modal-body-text text-center">
+            Вы уверены, что хотите убрать
+            билет <span>пассажира № 1?</span>
+          </p>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="modal-footer justify-content-center">
+          <button class="btn-yes">
+            Да
+          </button>
+          <button class="btn-close" data-bs-dismiss="modal">
+            Нет
+          </button>
         </div>
       </div>
     </div>
@@ -26,5 +32,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.modal-body {
+  &-text {
+    @include font($uni,$regular,24px,32.4px,$base);
+    span {
+      color: $blue-active;
+    }
+  }
+}
+.modal-footer {
+  .btn-yes, .btn-close {
+    @include font($uni,$regular,24px,32.4px,$deactivate);
+    background: none;
+    border: none;
+    outline: none;
+    width: unset;
+    height: unset;
+  }
+  .btn-yes:hover, .btn-close:hover {
+    @include animation;
+    color: $secondary;
+  }
+}
 </style>

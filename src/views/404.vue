2baf9cb53bf1997f0087e14b83e09736bf791603
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-20 18:33:26 +0300

    created 404.vue view

---
+++ src/views/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="body-app-white">
+    <Header/>
+    <LoginModal/>
+    <RegistrationModal/>
+    <ForgotPasswordModal/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import LoginModal from "@/components/LoginModal";
+export default {
+  name: "404",
+  components: {ForgotPasswordModal, RegistrationModal, Footer, Header, LoginModal}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.body-app-white {
+  background-color: $bg-app-light;
+}
+.footer {
+  margin-top: 72px;
+}
+</style>

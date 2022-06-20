b4d3b6b711252b7cc25cf9060c62416e3b96216f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-20 13:00:28 +0300

    created Payment-success-layout.vue

---
+++ src/components/Payment-success-layout.vue
@@ -0,0 +1,15 @@
+<template>
+<section class="payment-success-layout">
+
+</section>
+</template>
+
+<script>
+export default {
+  name: "Payment-success-layout"
+}
+</script>
+
+<style scoped>
+
+</style>

50b93e798277b38f5a4a318f7699f8937fe0d7d1
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-20 13:10:20 +0300

    created layout for Payment-success-layout.vue and added file payment-success.svg

--- src/components/Payment-success-layout.vue
+++ src/components/Payment-success-layout.vue
@@ -1,6 +1,26 @@
 <template>
 <section class="payment-success-layout">
-
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      <h1 class="title-section text-center">
+        Оплата успешно завершена
+      </h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <p class="description-section text-center">
+        Чек за покупку и билет, будут отправлены на указанную вами почту. Желаем Вам счастливого пути!
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center">
+      <img class="image-section" alt="успешная оплата" src="img/payment-success/payment-success.svg">
+    </div>
+  </div>
+</div>
 </section>
 </template>
 
@@ -10,6 +30,17 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.payment-success-layout {
+  .title-section {
+    @include font($uni,$bold,20px,27px,$base);
+    margin-bottom: 16px;
+  }
+  .description-section {
+    @include font($uni,$light,14px,18.9px,$base);
+    margin-bottom: 40px;
+  }
+}
 </style>

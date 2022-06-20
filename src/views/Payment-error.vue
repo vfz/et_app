9bda57b82238f3bebdc6cd999f22d3e80bf3e316
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-20 16:40:24 +0300

    imported components to Payment-error.vue

--- src/views/Payment-error.vue
+++ src/views/Payment-error.vue
@@ -1,13 +1,25 @@
 <template>
-<div class="body-app-white"></div>
+<div class="body-app-white">
+  <Header/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+  <Footer/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import Footer from "@/components/Footer";
 export default {
-  name: "Payment-error"
+  name: "Payment-error",
+  components: {Footer, ForgotPasswordModal, RegistrationModal, Header, LoginModal}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>

7f6a447bae5c18dd61ed76aea6fed877edc2d6d7
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-21 13:36:45 +0300

    added animation of text-decoration to nav-link when hover on element

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -135,6 +135,25 @@ export default {
           position: relative;
           color: $white;
         }
+        .dropdown-toggle:after {
+          border-top: 0 ;
+        }
+        .nav-link:after {
+          content: '';
+          position: absolute;
+          width: 100%;
+          transform: scaleX(0);
+          height: 2px;
+          bottom: 0;
+          left: 0;
+          background-color: $blue-color;
+          transform-origin: bottom right;
+          transition: transform 0.25s ease-out;
+        }
+        .nav-link:hover:after {
+          transform: scaleX(1);
+          transform-origin: bottom left;
+        }
       }
     }
   }

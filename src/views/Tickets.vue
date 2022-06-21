08c302aa9596007f9c4e99f14c1d9f091ce7bd2e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-21 17:09:23 +0300

    created Tickets.vue

---
+++ src/views/Tickets.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="body-app-white">
+    <Header/>
+    <Footer/>
+    <LoginModal/>
+    <RegistrationModal/>
+    <ForgotPasswordModal/>
+    <RemoveModal/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import RemoveModal from "@/components/RemoveModal";
+export default {
+  name: "Tickets",
+  components: {RemoveModal, ForgotPasswordModal, RegistrationModal, LoginModal, Footer, Header}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.body-app-white {
+  background-color: $bg-app-light;
+}
+.footer {
+  margin-top: 72px;
+}
+</style>

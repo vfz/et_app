3d3a20e89560729c0f931dfef3d8247aa4919065
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-22 10:06:09 +0300

    created SectionImage.vue and imported tickets.svg

---
+++ src/components/SectionImage.vue
@@ -0,0 +1,32 @@
+<template>
+<section class="image-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <img src="#" alt="#">
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: "SectionImage"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+
+.image-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  @media screen and (max-width: 767px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+}
+
+</style>

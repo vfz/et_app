da269793c83b0271530ee51944721fedfc1b054d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-24 12:39:42 +0300

    imported Header,Footer,LoginModal,RegistrationModal,ForgotPasswordModal into My-trips.vue

--- src/views/My-trips.vue
+++ src/views/My-trips.vue
@@ -1,12 +1,22 @@
 <template>
 <div class="body-app-white">
-
+  <Header/>
+  <Footer/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
 </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "My-trips"
+  name: "My-trips",
+  components: {ForgotPasswordModal, RegistrationModal, LoginModal, Footer, Header}
 }
 </script>
 

bc121f036abf3cb2fb4f61f7e4c00e66d28e6208
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-24 12:14:08 +0300

    created My-trips.vue view

---
+++ src/views/My-trips.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="body-app-white">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "My-trips"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.body-app-white {
+  background-color: $bg-app-light;
+  .btn {
+    @include font($uni,$regular,16px,21.6px,$white);
+    padding: 11px 47px;
+    border-radius: 8px;
+    border: none;
+    color: $white;
+    background-color: $blue-active;
+    margin-bottom: 72px;
+    text-transform: none;
+  }
+}
+</style>

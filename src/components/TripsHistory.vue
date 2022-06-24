2e0708045bb82cc95d913b243efefc00d5505595
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-24 13:14:50 +0300

    created TripsHistory.vue

---
+++ src/components/TripsHistory.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="trips-history-section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TripsHistory"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/variables.scss";
+@import "@/assets/font.scss";
+
+.trips-history-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  @media screen and (max-width: 767px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+}
+</style>

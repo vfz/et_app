0d4e15955884654a7b84a1792b122d79e10a3125
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-24 09:48:43 +0300

    imported Paths-final-amount from TicketOrderMobile.vue to Paths-final-amount.vue

--- src/components/mobile/TicketOrderMobile.vue
+++ src/components/mobile/TicketOrderMobile.vue
@@ -545,20 +545,14 @@
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12 d-flex justify-content-end">
-        <div class="paths-final-amount d-inline-block position-relative">
-          <div class="old-amount position-absolute">14 000₽</div>
-          Итого <span>11 000 ₽</span>
-        </div>
-      </div>
-    </div>
+    <PathsFinalAmount/>
   </div>
 </template>
 
 <script>
 import ArrowDownIcon from "@/components/icons/ArrowDownIcon";
 import CancelIcon from "@/components/icons/CancelIcon";
+import PathsFinalAmount from "@/components/mobile/TicketOrder/Paths-final-amount";
 export default {
   name: "TicketOrderMobile",
   data() {
@@ -574,6 +568,7 @@ export default {
     }
   },
   components: {
+    PathsFinalAmount,
     ArrowDownIcon, CancelIcon
   }
 }
@@ -814,17 +809,6 @@ export default {
   }
   .active {
     background: #F3F7FF;
-  }
-  .paths-final-amount {
-    margin-top: 24px;
-    @include font($uni,$bold,20px,27px,$blue-active);
-    text-transform: uppercase;
-    .old-amount {
-      @include font($uni,$bold,14px,18.9px,$secondary);
-      text-decoration: line-through;
-      top: -15px;
-      right: 0;
-    }
   }
 }
 </style>

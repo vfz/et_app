f07363c4427d37d27602d99ee3ebd6d23092ad01
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-24 09:44:27 +0300

    added paths final amount

--- src/components/mobile/TicketOrderMobile.vue
+++ src/components/mobile/TicketOrderMobile.vue
@@ -545,6 +545,14 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-12 d-flex justify-content-end">
+        <div class="paths-final-amount d-inline-block position-relative">
+          <div class="old-amount position-absolute">14 000₽</div>
+          Итого <span>11 000 ₽</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -806,6 +814,17 @@ export default {
   }
   .active {
     background: #F3F7FF;
+  }
+  .paths-final-amount {
+    margin-top: 24px;
+    @include font($uni,$bold,20px,27px,$blue-active);
+    text-transform: uppercase;
+    .old-amount {
+      @include font($uni,$bold,14px,18.9px,$secondary);
+      text-decoration: line-through;
+      top: -15px;
+      right: 0;
+    }
   }
 }
 </style>

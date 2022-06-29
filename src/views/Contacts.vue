af3bc4781eba8ced8767f658181287d38dbdb57a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-29 15:23:37 +0300

    imported components to Contacts.vue

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,13 +1,30 @@
 <template>
-
+<div class="body-app-white">
+  <Header/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+  <Footer/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "Contacts"
+  name: "Contacts",
+  components: { LoginModal,ForgotPasswordModal, RegistrationModal, Footer, Header}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.body-app-white {
+  background-color: $bg-app-light;
+}
 </style>

d6ef56318a8075ea4853a7d4d3c4472ac5be02db
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-29 17:29:32 +0300

    changed layout and style for ContactMapMobile.vue

--- src/components/mobile/Contacts/ContactMapMobile.vue
+++ src/components/mobile/Contacts/ContactMapMobile.vue
@@ -1,5 +1,71 @@
 <template>
+<section class="contact-map-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="contact-map-wrapper">
+          <div class="contact-map-info">
+            <h1 class="title-section">
+              Контакты
+            </h1>
+            <div class="contact-map-info-block">
+              <h2 class="title-info">
+                Адрес:
+              </h2>
+              <div class="address-info">
+                <h3 class="title-address">
+                  Главный офис:
+                </h3>
+                <p class="description-address">
+                  355035, г. Ставрополь, ул. Старомарьевское шоссе, 32ж
+                </p>
+              </div>
+              <div class="address-info">
+                <h3 class="title-address">
+                  Касса:
+                </h3>
+                <p class="description-address">
+                  355008, г. Ставрополь, ул. проспект Карла Маркса, 1а
+                </p>
+              </div>
+            </div>
+            <div class="contact-map-info-block">
+              <h2 class="title-info">
+                Почта:
+              </h2>
+              <p class="description">
+                info@evrotrans.net
+              </p>
+            </div>
+            <div class="contact-map-info-block">
+              <h2 class="title-info">
+                Телефон:
+              </h2>
+              <a href="tel:88007002099">
+                <p class="description">
+                  8 (800) 700 – 20 – 99
+                </p>
+              </a>
+            </div>
+            <div class="contact-map-info-block">
+              <h2 class="title-info">
+                Время работы:
+              </h2>
+              <p class="description">
+                Ежедневно, с 8:00 до 19:00
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="map">
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A925af31f9c09ee53d2bab03772075b968e64b2a235f466ab454485f53832734e&amp;source=constructor" width="100%" height="319" frameborder="0">
 
+    </iframe>
+  </div>
+</section>
 </template>
 
 <script>
@@ -8,6 +74,48 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../../../assets/variables";
+@import "../../../assets/font";
 
+.contact-map-section {
+  .contact-map-wrapper {
+    margin-left: 88px;
+    margin-right: 88px;
+    @media screen and (max-width: 991px) {
+      margin-left: 20px;
+      margin-right: 20px;
+    }
+    .contact-map-info {
+      .title-section {
+        @include font($uni,$bold,20px,27px,$base);
+        margin-bottom: 24px;
+      }
+      &-block {
+        margin-bottom: 32px;
+        .title-info {
+          @include font($uni,$bold,18px,24.3px,$base);
+          margin-bottom: 12px;
+        }
+        .address-info {
+          margin-bottom: 12px;
+        }
+        .address-info:last-child {
+          margin-bottom: 0;
+        }
+        .title-address {
+          @include font($uni,$regular,14px,18.9px,$secondary);
+          margin-bottom: 8px;
+        }
+        .description-address, .description {
+          @include font($uni,$light,14px,18.9px,$base);
+          margin-bottom: 0;
+        }
+      }
+    }
+  }
+  .map {
+    margin-bottom: 40px;
+  }
+}
 </style>

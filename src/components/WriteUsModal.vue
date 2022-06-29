155bb2d2b75edc726bd188d99077a6a5ffce7889
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-29 09:36:28 +0300

    changed place of modal-title

--- src/components/WriteUsModal.vue
+++ src/components/WriteUsModal.vue
@@ -4,11 +4,10 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <h5 class="modal-title" id="WriteUsModalLabel">Помогите стать нам лучше</h5>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

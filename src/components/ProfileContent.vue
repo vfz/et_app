ddea170aef6a063def326765419c2d584b5b9740
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-30 14:24:41 +0300

    created profile-section into ProfileContent.vue

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <SectionTitle title="Личный кабинет" description="Добро пожаловать в личный кабинет. Здесь, вы можете редактировать информацию о себе и пассажирах, на которых Вы оформляли билет, а также изменить свой пароль."/>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-10">
-          <ProfileData/>
-          <ProfileAutocomplete/>
-        </div>
-        <div class="col-2">
-          <ProfileAvatar/>
+    <section class="profile-section">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-10">
+            <ProfileData/>
+            <ProfileAutocomplete/>
+          </div>
+          <div class="col-2">
+            <ProfileAvatar/>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -27,7 +29,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.description-section {
-
+.profile-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  margin-top: 40px;
+  @media screen and (max-width: 991px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 }
 </style>

5f9060052cf91b2d28b9bad0accfa10ba301221f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-30 13:31:00 +0300

    created DataProfile.vue and imported into ProfileContent.vue

---
+++ src/components/DataProfile.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DataProfile"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <SectionTitle title="Личный кабинет" description="Добро пожаловать в личный кабинет. Здесь, вы можете редактировать информацию о себе и пассажирах, на которых Вы оформляли билет, а также изменить свой пароль."/>
+    <DataProfile/>
   </div>
 </template>
 
 <script>
 import SectionTitle from "@/components/SectionTitle";
+import DataProfile from "@/components/DataProfile";
 export default {
   name: "ProfileContent",
-  components: {SectionTitle}
+  components: {DataProfile, SectionTitle}
 }
 </script>
 

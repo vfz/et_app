9ead49ad4bc7347a201ef29ff4deb25420f27c2d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-06-30 13:32:31 +0300

    created columns in ProfileContent.vue

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <SectionTitle title="Личный кабинет" description="Добро пожаловать в личный кабинет. Здесь, вы можете редактировать информацию о себе и пассажирах, на которых Вы оформляли билет, а также изменить свой пароль."/>
-    <DataProfile/>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-10">
+          <DataProfile/>
+        </div>
+        <div class="col-2">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

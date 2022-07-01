49ad79b0a3068e0217e6d39209a28a07932866f9
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-01 15:34:47 +0300

    created layout for Ticket-refund.vue

--- src/views/Ticket-refund.vue
+++ src/views/Ticket-refund.vue
@@ -1,13 +1,41 @@
 <template>
+<div class="body-app-white">
+  <Header/>
+  <SectionTitle title="Возврат билета"/>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-10">
 
+      </div>
+    </div>
+  </div>
+  <Footer/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import SectionTitle from "@/components/SectionTitle";
 export default {
-  name: "Ticket-refund"
+  name: "Ticket-refund",
+  components: {SectionTitle, ForgotPasswordModal, RegistrationModal, LoginModal, Footer, Header}
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.body-app-white {
+  background-color: $bg-app-light;
+}
+.footer {
+  margin-top: 72px;
+}
 </style>

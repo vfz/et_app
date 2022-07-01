68072ac3de2bdcf4485fe6485528bd238c28a659
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-01 09:12:45 +0300

    Created avatar uploading into ProfileAvatar.vue

--- src/components/ProfileAvatar.vue
+++ src/components/ProfileAvatar.vue
@@ -1,12 +1,25 @@
 <template>
 <div class="avatar-section">
-
+  <img v-bind:src="url" class="avatar" alt="аватар">
+  <input @change="getNewAvatar" accept="image/*" type="file" >
 </div>
 </template>
 
 <script>
 export default {
-  name: "ProfileAvatar"
+  name: "ProfileAvatar",
+  data(){
+    return {
+      url : '/img/profile/placeholder.png',
+    }
+  },
+  methods: {
+    getNewAvatar(event) {
+      const file = event.target.files[0]
+      this.url = URL.createObjectURL(file);
+      return this.url
+    }
+  }
 }
 </script>
 

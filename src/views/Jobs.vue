d18aec355ecfea967b8a548dd14b47eb7217e21a
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-04 15:30:55 +0300

    imported core components into Jobs.vue

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,13 +1,51 @@
 <template>
-
+<div class="body-app-white">
+  <Header/>
+  <Footer/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "Jobs"
+  name: "Jobs",
+  components: {ForgotPasswordModal, RegistrationModal, LoginModal, Footer, Header}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.body-app-white {
+  background-color: $bg-app-light;
+  .refund-info {
+    margin-left: 88px;
+    margin-right: 88px;
+    @media screen and (max-width: 991px) {
+      margin-left: 20px;
+      margin-right: 20px;
+    }
+  }
+  .image-section {
+    width: 100%;
+    max-width: 572px;
+  }
+  .btn {
+    @include font($uni,$regular,24px,32.4px,$white);
+    padding: 23px 83px;
+    margin-top: 40px;
+    border-radius: 16px;
+  }
+}
+.footer {
+  margin-top: 72px;
+}
 </style>

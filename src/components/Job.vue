86b5078e44f1bcc7661c1513a8da9b6e38c45690
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-04 15:44:42 +0300

    created layout for Job.vue

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -1,5 +1,24 @@
 <template>
-
+<div class="col-12">
+  <div class="job-wrapper">
+    <div class="job-main-info">
+      <h3 class="job-title"></h3>
+      <div class="job-short">
+        <span class="job-payment">
+          зп от 30 000₽
+        </span>
+        /
+        <span class="job-city">Ставрополь,</span>
+        <span class="job-place">Главный офис</span>
+      </div>
+    </div>
+    <div class="job-details">
+      <button class="btn-primary btn">
+        Подробнее
+      </button>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>

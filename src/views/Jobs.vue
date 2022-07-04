45ad4e353633ef626b50c4c71c0436f36bee67e1
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-04 16:52:45 +0300

    added data  id for job

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -5,7 +5,7 @@
   <section class="jobs-list-section">
     <div class="container-fluid">
       <div class="row">
-        <Job title="Бухгалтер" payment="30 000" city="Ставрополь" place="Главный офис"/>
+        <Job id="1" title="Бухгалтер" payment="30 000" city="Ставрополь" place="Главный офис"/>
       </div>
     </div>
   </section>

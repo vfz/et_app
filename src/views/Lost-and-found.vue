ea59bdf7ad07cd5dd23c85dbd8d58e77c7a57799
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-04 13:46:12 +0300

    imported components core components into Lost-and-found.vue

--- src/views/Lost-and-found.vue
+++ src/views/Lost-and-found.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <div class="body-app-white">
+    <Header/>
+    <SectionTitle/>
+    <Footer/>
+    <LoginModal/>
+    <RegistrationModal/>
+    <ForgotPasswordModal/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import SectionTitle from "@/components/SectionTitle";
+import Footer from "@/components/Footer";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "Lost-and-found"
+  name: "Lost-and-found",
+  components: {ForgotPasswordModal, RegistrationModal, LoginModal, Footer, SectionTitle, Header}
 }
 </script>
 

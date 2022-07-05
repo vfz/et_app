366440ac726380ad5ffd033c21936e5efe594b81
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-05 20:38:30 +0300

    added layout and style for FileUploader.vue

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -1,5 +1,16 @@
 <template>
+  <div class="row file-uploader-section">
+    <div class="col-12">
+      <h2 class="title-section">
+        Резюме
+      </h2>
+    </div>
+    <div class="col-12">
+      <div class="form-wrapper">
 
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.file-uploader-section {
+  margin-top: 40px;
+  .title-section {
+    @include font($uni,$bold,24px,32.4px,$base);
+    margin-bottom: 32px;
+  }
+}
 </style>

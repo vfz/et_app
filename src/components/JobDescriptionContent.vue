f69e04ba7f22559d4eddfa485ff24f0a87d27b79
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-05 18:51:40 +0300

    changed from h3 to h2 title. Added description-items

--- src/components/JobDescriptionContent.vue
+++ src/components/JobDescriptionContent.vue
@@ -16,9 +16,9 @@
       <div class="row">
         <div class="col-12">
           <div class="job-main-info text-center text-lg-start d-flex flex-column">
-            <h3 class="job-title">
+            <h2 class="job-title">
               {{title}}
-            </h3>
+            </h2>
             <div class="job-short">
         <span class="job-payment">
           зп от {{payment}}₽
@@ -28,6 +28,94 @@
             </div>
           </div>
         </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <div class="description-item">
+            <h3 class="title-description">
+              Описание
+            </h3>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+            </p>
+            <h4 class="title-list">
+              Вам предстоит:
+            </h4>
+            <ul class="list-group">
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+            </ul>
+          </div>
+          <div class="description-item">
+            <h3 class="title-description">
+              Описание
+            </h3>
+            <h4 class="title-list">
+              Вам предстоит:
+            </h4>
+            <ul class="list-group">
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+            </ul>
+            <h4 class="title-list">
+              Вам предстоит:
+            </h4>
+            <ul class="list-group">
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+            </ul>
+          </div>
+          <div class="description-item">
+            <h3 class="title-description">
+              Описание
+            </h3>
+            <ul class="list-group">
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+              <li class="list-group-item">
+                — Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-6"></div>
       </div>
     </div>
   </section>

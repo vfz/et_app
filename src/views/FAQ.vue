107a1faee481f4367a3e6bc0aeb2df7f91300e95
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-06 15:56:32 +0300

    Imported components and added style for FAQ.vue

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,13 +1,37 @@
 <template>
-
+<div class="body-app-white">
+  <Header/>
+  <SectionTitle title="Вопросы и ответы"/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+  <RemoveModal/>
+  <Footer/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import SectionTitle from "@/components/SectionTitle";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import RemoveModal from "@/components/RemoveModal";
 export default {
-  name: "FAQ"
+  name: "FAQ",
+  components: {RemoveModal, ForgotPasswordModal, RegistrationModal, LoginModal, SectionTitle, Footer, Header}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.body-app-white {
+  background-color: $bg-app-light;
+}
+.footer {
+  margin-top: 72px;
+}
 </style>

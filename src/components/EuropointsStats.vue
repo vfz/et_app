93095ce43e30374f3263b9922b6709ebcf03e47d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-07 17:31:08 +0300

    created layout for stats. Added style for stats

--- src/components/EuropointsStats.vue
+++ src/components/EuropointsStats.vue
@@ -1,5 +1,21 @@
 <template>
-
+<div class="row">
+  <div class="col">
+    <div class="stats">
+      <h3 class="stats-title">
+        За прошлый месяц
+      </h3>
+      <div class="stats-number stats-plus">
+        +440
+      </div>
+      <div class="stats-footer">
+        Евробаллов
+      </div>
+    </div>
+  </div>
+  <div class="col"></div>
+  <div class="col"></div>
+</div>
 </template>
 
 <script>
@@ -8,6 +24,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.stats {
+  &-title {
+    @include font($uni,$regular,14px,18.9px,$base);
+    text-align: center;
+    margin-bottom: 37px;
+  }
+  &-number {
+    @include font($uni,$bold,24px,32.4px,$blue-active);
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  &-footer {
+    @include font($uni,$regular,14px,18.9px,#B5BDDB);
+    text-align: center;
+  }
+}
 </style>

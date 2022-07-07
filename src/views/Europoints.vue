5b009f169c46d0903705490663c9df50f12afcd9
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-07 16:49:43 +0300

    added core components and style for Europoints.vue

--- src/views/Europoints.vue
+++ src/views/Europoints.vue
@@ -1,13 +1,32 @@
 <template>
-
+<div class="body-app-white">
+  <Header/>
+  <SectionTitle title="Евробаллы"/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import SectionTitle from "@/components/SectionTitle";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "Europoints"
+  name: "Europoints",
+  components: {ForgotPasswordModal, RegistrationModal, LoginModal, SectionTitle, Header}
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
+.body-app-white {
+  background-color: $bg-app-light;
+}
+.footer {
+  margin-top: 72px;
+}
 </style>

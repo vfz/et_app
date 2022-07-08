27bdb1b7f6f250db7d513da53d587443263a13a4
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-08 18:57:29 +0300

    added list-group-item and added style. Added margin-bottom for second title-section in AgentsDescription.vue

--- src/components/AgentsDescription.vue
+++ src/components/AgentsDescription.vue
@@ -8,6 +8,20 @@
       <span> (Описание о том, что это и как это работает) </span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
     </p>
   </div>
+  <div class="col-6">
+    <h2 class="title-section">
+      У вас появится возможность:
+    </h2>
+    <ul class="list-group">
+      <li class="list-group-item">
+        – Lorem ipsum dolor sit amet,
+      </li>
+      <li class="list-group-item">
+        – Lorem ipsum dolor sit amet,
+      </li>
+    </ul>
+  </div>
+  <div class="col-6"></div>
 </div>
 </template>
 
@@ -22,6 +36,11 @@ export default {
 @import "src/assets/font.scss";
 
 .agents-description-section {
+  .col-6 {
+    .title-section {
+      margin-bottom: 16px;
+    }
+  }
   .title-section {
     @include font($uni,$bold,24px,32.4px,$base);
     margin-bottom: 32px;
@@ -30,6 +49,15 @@ export default {
     @include font($uni,$light,18px,24.3px,$base);
     span {
       font-weight: $regular;
+    }
+  }
+  .list-group {
+    &-item {
+      @include font($uni,$light,18px,24.3px,$base);
+      background: none;
+      border: none;
+      padding-left: 0;
+      padding-right: 0;
     }
   }
 }

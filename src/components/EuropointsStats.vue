ac129e0fe2aa047d4db967f57d9fa860d79e863e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-08 14:48:53 +0300

    changed style for mobile

--- src/components/EuropointsStats.vue
+++ src/components/EuropointsStats.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="row europoints-stats-section">
-  <div class="col">
+  <div class="col-12 col-md">
     <div class="stats d-flex flex-column justify-content-between align-items-center">
       <h3 class="stats-title">
         За прошлый месяц
@@ -13,7 +13,7 @@
       </div>
     </div>
   </div>
-  <div class="col">
+  <div class="col-12 col-md">
     <div class="stats d-flex flex-column justify-content-between align-items-center">
       <h3 class="stats-title">
         На данный момент
@@ -26,7 +26,7 @@
       </div>
     </div>
   </div>
-  <div class="col">
+  <div class="col-12 col-md">
     <div class="stats d-flex flex-column justify-content-between align-items-center">
       <h3 class="stats-title">
         За весь период
@@ -53,12 +53,22 @@ export default {
 @import "src/assets/font.scss";
 .europoints-stats-section {
   margin-bottom: 47px;
+  @media screen and (max-width: 767px) {
+    margin-bottom: 0;
+  }
   .stats {
     height: 100%;
+    @media screen and (max-width: 767px) {
+      margin-bottom: 32px;
+      height: auto;
+    }
     &-title {
       @include font($uni,$regular,14px,18.9px,$base);
       text-align: center;
       margin-bottom: 37px;
+      @media screen and (max-width: 767px) {
+        margin-bottom: 24px;
+      }
     }
     &-number {
       @include font($uni,$bold,24px,32.4px,$blue-active);
@@ -69,6 +79,10 @@ export default {
       font-size: 48px;
       line-height: 64.8px;
       color: $base;
+      @media screen and (max-width: 767px) {
+        margin-bottom: 36px;
+        font-size: 36px;
+      }
     }
     &-amount {
       color: $secondary;

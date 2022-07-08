7ca54fd597d0586d34f80717a9a49fd804343879
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-08 11:36:47 +0300

    added collapse buttons and style

--- src/components/EuropointsAbout.vue
+++ src/components/EuropointsAbout.vue
@@ -5,12 +5,48 @@
       Евробаллы — это полезная бонусная программа, с помощью которой, Вы сможете минимизировать свои затраты на приобретение билетов или же полностью оплатить билет. Покупая билеты на нашем сайте или с помощью нашего приложения, Вы получаете евробаллы (10% от стоимости билета). Евробаллы начисляются после завершения поездки. Срок жизни евробаллов — неограничен.
     </p>
   </div>
+  <div class="col-12">
+    <div class="details w-100 d-flex justify-content-between" v-bind:class="{'d-none': isShowDetails}">
+      <button class="d-flex w-100 align-items-center justify-content-between" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEuropoints" aria-expanded="false" aria-controls="collapseEuropoints" v-on:click="showCollapse()">
+        Подробнее
+        <ArrowDownIcon color="#283256"/>
+      </button>
+    </div>
+    <div class="details details-hide w-100 d-flex justify-content-between" v-bind:class="{'d-none': isHideDetails}">
+      <button class="d-flex w-100 align-items-center justify-content-between" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEuropoints" aria-expanded="false" aria-controls="collapseEuropoints" v-on:click="showCollapse()">
+        Свернуть
+        <ArrowDownIcon color="#283256"/>
+      </button>
+    </div>
+    <div class="collapse" id="collapseEuropoints">
+      <div class="collapse-wrapper">
+
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
+import CancelIcon from "@/components/icons/CancelIcon";
+import ArrowDownIcon from "@/components/icons/ArrowDownIcon";
+import ArrowPathIcon from "@/components/icons/ArrowPathIcon";
+
 export default {
-  name: "EuropointsAbout"
+  name: "EuropointsAbout",
+  components: {ArrowPathIcon, CancelIcon, ArrowDownIcon},
+  data() {
+    return {
+      isShowDetails: false,
+      isHideDetails: true,
+    }
+  },
+  methods: {
+    showCollapse: function () {
+      this.isHideDetails = !this.isHideDetails;
+      this.isShowDetails = !this.isShowDetails;
+    }
+  }
 }
 </script>
 
@@ -22,6 +58,28 @@ export default {
   margin-bottom: 32px;
   .europoints-description {
     @include font($uni,$light,18px,24.3px,$base);
+  }
+  .details {
+    padding-bottom: 16px;
+    border-bottom: 1px solid #E1EEF6;
+    button {
+      background: none;
+      outline: none;
+      border: none;
+      @include font($uni,$bold,24px,32.4px,$base);
+      svg {
+
+      }
+    }
+  }
+  .details-hide {
+    @include animation;
+    border-bottom: none;
+    button {
+      svg {
+        transform: rotate(180deg);
+      }
+    }
   }
 }
 </style>

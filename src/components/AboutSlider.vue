ee578e824ff19cc7ddacbe21e7ae58cb6cbb591d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 15:56:37 +0300

    added layout of nav in AboutSlider.vue

--- src/components/AboutSlider.vue
+++ src/components/AboutSlider.vue
@@ -8,6 +8,34 @@
         </h2>
       </div>
     </div>
+    <div class="row">
+      <div class="col-3">
+        <ul class="nav flex-column nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="TwoFloors-tab" data-bs-toggle="tab" data-bs-target="#TwoFloors" type="button" role="tab" aria-controls="TwoFloors" aria-selected="true">Двухэтажные автобусы</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="semiFloor-tab" data-bs-toggle="tab" data-bs-target="#semiFloor" type="button" role="tab" aria-controls="semiFloor" aria-selected="false">Полутораэтажные автобусы</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="oneFloor-tab" data-bs-toggle="tab" data-bs-target="#oneFloor" type="button" role="tab" aria-controls="oneFloor" aria-selected="false">Одноэтажные автобусы</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="mercedesSprinter-tab" data-bs-toggle="tab" data-bs-target="#mercedesSprinter" type="button" role="tab" aria-controls="mercedesSprinter" aria-selected="false">Mercedes Sprinter </button>
+          </li>
+        </ul>
+      </div>
+      <div class="col-9">
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="twoFloors" role="tabpanel" aria-labelledby="twoFloors-tab">
+            hello
+          </div>
+          <div class="tab-pane fade" id="semiFloor" role="tabpanel" aria-labelledby="semiFloor-tab"></div>
+          <div class="tab-pane fade" id="oneFloor" role="tabpanel" aria-labelledby="oneFloor-tab"></div>
+          <div class="tab-pane fade" id="mercedesSprinter" role="tabpanel" aria-labelledby="mercedesSprinter-tab"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 </template>
@@ -36,6 +64,20 @@ export default {
     @media screen and (max-width: 768px) {
       font-size: 18px;
       line-height: 24.3px;
+    }
+  }
+  .nav {
+    &-item {
+
+    }
+    &-link {
+      @include font($uni,$bold,18px,24.3px,$deactivate);
+      padding-left: 0;
+      padding-right: 0;
+      text-transform: none;
+    }
+    .active {
+      color: $blue-active;
     }
   }
 }

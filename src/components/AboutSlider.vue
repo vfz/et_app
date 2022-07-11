b89e12d7753dd572c9fd490db11127fe3ee3ecd2
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 15:22:32 +0300

    added title-section into AboutSlider.vue

--- src/components/AboutSlider.vue
+++ src/components/AboutSlider.vue
@@ -1,5 +1,15 @@
 <template>
-
+<section class="about-slider-section">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="title-section">
+          Наш автопарк
+        </h2>
+      </div>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
@@ -8,6 +18,25 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.about-slider-section {
+  margin-left: 88px;
+  margin-right: 88px;
+  @media screen and (max-width: 991px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+  .title-section {
+    @include font($uni,$bold,24px,32.4px,$base);
+    margin-bottom: 32px;
+    margin-top: 40px;
+    @media screen and (max-width: 768px) {
+      font-size: 18px;
+      line-height: 24.3px;
+    }
+  }
+}
 </style>

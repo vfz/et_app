089b7d687fdf2cb8a7372ee5e50f2c809f4c4a42
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 14:06:11 +0300

    fixed col name

--- src/components/AboutReasons.vue
+++ src/components/AboutReasons.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-xl-6 ccol-xxl-7">
+      <div class="col-xl-6 col-xxl-7">
         <div class="description-section">
           <p>
             Excepteur sint occaecat  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.

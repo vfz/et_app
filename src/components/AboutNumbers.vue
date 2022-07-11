c2fe2f24cb05a25eeadb9a4b46ca42a97b1fa050
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 13:17:28 +0300

    added style for mobile in AboutNumbers.vue

--- src/components/AboutNumbers.vue
+++ src/components/AboutNumbers.vue
@@ -50,7 +50,7 @@
           </h3>
         </div>
       </div>
-      <div class="row mt-4 align-items-end">
+      <div class="row align-items-end">
         <div class="col-12 col-lg">
           <div class="number">
             10
@@ -111,25 +111,50 @@ export default {
     padding-left: 20px;
     padding-right: 20px;
   }
+  @media screen and (max-width: 768px) {
+    padding-top: 24px;
+    padding-bottom: 24px;
+  }
   .title-section {
     @include font($uni,$bold,24px,32.4px,$white);
     margin-bottom: 32px;
+    @media screen and (max-width: 768px) {
+      margin-bottom: 16px;
+    }
   }
   .row:first-child {
     margin-top: 0;
   }
   .row {
     margin-top: 32px;
+    @media screen and (max-width: 768px) {
+      margin-top: 0;
+    }
     .col-12 {
+      @media screen and (max-width: 768px) {
+        margin-bottom: 16px;
+      }
       .number {
         @include font($uni,$bold,48px,64.8px,$white);
         margin-bottom: 8px;
+        @media screen and (max-width: 768px) {
+          font-size: 36px;
+          line-height: 48.6px;
+        }
       }
       .title {
         @include font($uni,$light,18px,24.3px,$white);
+        @media screen and (max-width: 768px) {
+          font-size: 14px;
+          line-height: 18.9px;
+        }
       }
       .more {
         @include font($uni,$light,18px,24.3px,#B5BDDB);
+        @media screen and (max-width: 768px) {
+          font-size: 14px;
+          line-height: 18.9px;
+        }
       }
     }
   }

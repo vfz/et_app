16095b272b8b0da656d9774483e2390980238e39
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 10:13:31 +0300

    added core layout for About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,44 @@
 <template>
+<div class="body-app-white">
+  <Header/>
+  <SectionTitle title="О нас"/>
 
+  <Footer/>
+  <LoginModal/>
+  <RegistrationModal/>
+  <ForgotPasswordModal/>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import SectionTitle from "@/components/SectionTitle";
+import LoginModal from "@/components/LoginModal";
+import RegistrationModal from "@/components/RegistrationModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
 export default {
-  name: "About"
+  name: "About",
+  components: {ForgotPasswordModal, RegistrationModal, LoginModal, SectionTitle, Footer, Header}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "src/assets/variables.scss";
+@import "src/assets/font.scss";
 
+.body-app-white {
+  background-color: $bg-app-light;
+  .agents-section {
+    margin-left: 88px;
+    margin-right: 88px;
+    @media screen and (max-width: 991px) {
+      margin-left: 20px;
+      margin-right: 20px;
+    }
+  }
+}
+.footer {
+  margin-top: 72px;
+}
 </style>

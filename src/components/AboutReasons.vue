4408b3e9a2bb3522ffaa9cdd1ac34c5ff9536d8e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-11 13:37:41 +0300

    added description section

--- src/components/AboutReasons.vue
+++ src/components/AboutReasons.vue
@@ -10,7 +10,11 @@
     </div>
     <div class="row">
       <div class="col-xl-7">
-
+        <div class="description-section">
+          <p>
+            Excepteur sint occaecat  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
       <div class="col-xl-5">
 
@@ -41,6 +45,13 @@ export default {
     @include font($uni,$bold,24px,32.4px,$base);
     margin-bottom: 32px;
     margin-top: 40px;
+  }
+  .description-section {
+    @include font($uni,$light,18px,24.3px,#000);
+    margin-bottom: 32px;
+    p {
+      margin-bottom: 0;
+    }
   }
 }
 </style>

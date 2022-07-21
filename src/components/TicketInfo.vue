797381a6bae3f726044d04dfb08e29eedcfbe5e7
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-21 22:07:07 +0300

    added carousel-bootstrap for TicketInfo.vue

--- src/components/TicketInfo.vue
+++ src/components/TicketInfo.vue
@@ -4,8 +4,199 @@
     <h3 v-if="isMobile()" class="title-card">
       Информация о билете
     </h3>
-<!--    TODO доделать слайдер-->
-    <div class="info-ticket-wrapper">
+    <div v-if="isMobile()" class="carousel-bootstrap">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 p-0">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="info-ticket-wrapper">
+                    <div class="path-info-ticket">
+                      <h4 class="path-info-ticket-title">
+                        Туда
+                      </h4>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Маршрут:
+                      </h5>
+                      <div class="path-info-cities">
+                        <span class="path-info-city-from">Ставрополь</span>
+                        <ArrowPathIcon class="arrow-path-icon"/>
+                        <span class="path-info-city-to">Москва</span>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Отправление:
+                      </h5>
+                      <div class="path-info-from">
+                        <div class="path-info-from-datetime">
+                <span class="path-info-from-datetime-date">
+                  30.01.2020
+                </span>
+                          в
+                          <span class="path-info-from-datetime-time">
+                  15:30
+                </span>
+                        </div>
+                        <div class="path-info-from-place d-flex flex-column">
+                <span class="path-info-from-place-name">
+                  Железнодорожный вокзал
+                </span>
+                          <span class="path-info-from-datetime-address">
+                  ул. Вокзальная, 15
+                </span>
+                        </div>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Прибытие:
+                      </h5>
+                      <div class="path-info-to">
+                        <div class="path-info-to-datetime">
+                <span class="path-info-to-datetime-date">
+                  30.01.2020
+                </span>
+                          в
+                          <span class="path-info-to-datetime-time">
+                  15:30
+                </span>
+                        </div>
+                        <div class="path-info-to-place d-flex flex-column">
+                <span class="path-info-from-place-name">
+                  Железнодорожный вокзал
+                </span>
+                          <span class="path-info-from-datetime-address">
+                  ул. Вокзальная, 15
+                </span>
+                        </div>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Места
+                      </h5>
+                      <div class="path-info-places">
+              <span class="path-info-place-number">
+                33,
+              </span>
+                        <span class="path-info-place-number">
+                34,
+              </span>
+                        <span class="path-info-place-number">
+                35,
+              </span>
+                        <span class="path-info-place-number">
+                36,
+              </span>
+                      </div>
+                      <div class="path-info-sum">
+                        Сумма заказа <span class="path-info-sum-number">7 000</span>₽
+                      </div>
+                    </div>
+                    <div class="point-minus">
+                      - 3000₽ (Евробаллы)
+                    </div>
+                    <div class="paths-final-amount d-inline-block position-relative">
+                      <div class="old-amount position-absolute d-none">14 000₽</div>
+                      Итого <span>14 000 ₽</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="info-ticket-wrapper">
+                    <div class="path-info-ticket">
+                      <h4 class="path-info-ticket-title">
+                        Обратно
+                      </h4>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Маршрут:
+                      </h5>
+                      <div class="path-info-cities">
+                        <span class="path-info-city-from">Москва</span>
+                        <ArrowPathIcon class="arrow-path-icon"/>
+                        <span class="path-info-city-to">Ставрополь</span>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Отправление:
+                      </h5>
+                      <div class="path-info-from">
+                        <div class="path-info-from-datetime">
+                <span class="path-info-from-datetime-date">
+                  10.02.2020
+                </span>
+                          в
+                          <span class="path-info-from-datetime-time">
+                  20:45
+                </span>
+                        </div>
+                        <div class="path-info-from-place d-flex flex-column">
+                <span class="path-info-from-place-name">
+                  Автостанция “Орехово”
+                </span>
+                          <span class="path-info-from-datetime-address">
+                  ул. Шипиловский проезд, 12
+                </span>
+                        </div>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Прибытие:
+                      </h5>
+                      <div class="path-info-to">
+                        <div class="path-info-to-datetime">
+                <span class="path-info-to-datetime-date">
+                  11.02.2020
+                </span>
+                          в
+                          <span class="path-info-to-datetime-time">
+                  15:30
+                </span>
+                        </div>
+                        <div class="path-info-to-place d-flex flex-column">
+                <span class="path-info-from-place-name">
+                  Железнодорожный вокзал
+                </span>
+                          <span class="path-info-from-datetime-address">
+                  ул. Вокзальная, 15
+                </span>
+                        </div>
+                      </div>
+                      <h5 class="path-info-ticket-title-paragraph">
+                        Места
+                      </h5>
+                      <div class="path-info-places">
+              <span class="path-info-place-number">
+                16,
+              </span>
+                        <span class="path-info-place-number">
+                17,
+              </span>
+                        <span class="path-info-place-number">
+                18,
+              </span>
+                        <span class="path-info-place-number">
+                19,
+              </span>
+                      </div>
+                      <div class="path-info-sum">
+                        Сумма заказа <span class="path-info-sum-number">7 000</span>₽
+                      </div>
+                    </div>
+                    <div class="point-minus">
+                      - 3000₽ (Евробаллы)
+                    </div>
+                    <div class="paths-final-amount d-inline-block position-relative">
+                      <div class="old-amount position-absolute d-none">14 000₽</div>
+                      Итого <span>14 000 ₽</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="!isMobile()" class="info-ticket-wrapper">
       <h3 v-if="!isMobile()" class="title-card">
         Информация о билете
       </h3>
@@ -195,6 +386,23 @@ export default {
   @include font($uni,$bold,18px,24.3px,$base);
   margin-bottom: 16px;
   margin-top: 24px;
+}
+.carousel-bootstrap {
+  .container-fluid {
+    padding: 0;
+  }
+  .carousel-indicators {
+    margin-bottom: 0;
+  }
+  .carousel-indicators .active {
+    background-color: $base;
+  }
+  .carousel-indicators button {
+    background-color: $deactivate;
+  }
+  .carousel-inner {
+    padding: 12px 12px 28px;
+  }
 }
 .info-ticket-wrapper {
   background-color: $white;

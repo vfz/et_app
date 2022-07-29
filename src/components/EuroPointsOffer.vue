7552c99b8b3754f8cbe3b88ac732103e3fe948c6
Daniil Bobrov <bobdaniil@yandex.ru>
2022-07-29 15:29:53 +0300

    created collapse for EuroPointsOffer.vue

--- src/components/EuroPointsOffer.vue
+++ src/components/EuroPointsOffer.vue
@@ -3,9 +3,12 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-12">
-      <p class="description-section">
-        <span>Зарегистрированные пользователи имеют ряд преимуществ.</span> Одно из них — использование системы “евробаллов”,  с помощью которой, Вы сможете минимизировать свои затраты на приобретение билетов или же полностью оплатить билет. Если Вы ещё не зарегистрированны, Вы можете это сделать <a href="#">здесь.</a>
-      </p>
+        <a class="description-section pointer-event" data-bs-toggle="collapse" href="#collapseEuropoints" aria-expanded="false" aria-controls="collapseEuropoints">
+          Зарегистрированные пользователи имеют ряд преимуществ...
+        </a>
+      <div class="collapse" id="collapseEuropoints">
+        Одно из них — использование системы “евробаллов”,  с помощью которой, Вы сможете минимизировать свои затраты на приобретение билетов или же полностью оплатить билет. Если Вы ещё не зарегистрированны, Вы можете это сделать <a href="#">здесь.</a>
+      </div>
     </div>
   </div>
 </div>
@@ -32,15 +35,27 @@ export default {
   }
   .description-section {
     @include font($uni,$light,18px,24.3px,$base);
-    span {
-      color: $secondary;
-      font-weight: normal;
-    }
     a {
       color: $blue-color;
       font-weight: normal;
       text-decoration: underline $blue-color;
     }
+    @media screen and (max-width: 767px) {
+      font-size: 14px;
+    }
+  }
+  .description-section:hover {
+    @include animation;
+    color: $blue-color;
+  }
+  .collapse {
+    @include font($uni,$regular,18px,24.3px,$secondary);
+    @media screen and (max-width: 767px) {
+      font-size: 14px;
+    }
+  }
+  .collapsing {
+    @include font($uni,$regular,18px,24.3px,$secondary);
     @media screen and (max-width: 767px) {
       font-size: 14px;
     }

ed040a97d2f6c2434b6c4290f6d994345528bb6b
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-01 18:33:56 +0300

    changed collapse style for EuroPointsOffer.vue

--- src/components/EuroPointsOffer.vue
+++ src/components/EuroPointsOffer.vue
@@ -3,9 +3,9 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-12">
-        <a class="description-section pointer-event" data-bs-toggle="collapse" href="#collapseEuropoints" aria-expanded="false" aria-controls="collapseEuropoints">
-          Зарегистрированные пользователи имеют ряд преимуществ...
-        </a>
+      <a class="description-section pointer-event" data-bs-toggle="collapse" href="#collapseEuropoints" aria-expanded="false" aria-controls="collapseEuropoints">
+        Зарегистрированные пользователи имеют ряд преимуществ...
+      </a>
       <div class="collapse" id="collapseEuropoints">
         Одно из них — использование системы “евробаллов”,  с помощью которой, Вы сможете минимизировать свои затраты на приобретение билетов или же полностью оплатить билет. Если Вы ещё не зарегистрированны, Вы можете это сделать <a href="#">здесь.</a>
       </div>
@@ -34,7 +34,7 @@ export default {
     margin-right: 8px;
   }
   .description-section {
-    @include font($uni,$light,18px,24.3px,$base);
+    @include font($uni,$regular,18px,24.3px,$base);
     a {
       color: $blue-color;
       font-weight: normal;
@@ -49,15 +49,25 @@ export default {
     color: $blue-color;
   }
   .collapse {
-    @include font($uni,$regular,18px,24.3px,$secondary);
+    @include font($uni,$light,18px,24.3px,$base);
     @media screen and (max-width: 767px) {
       font-size: 14px;
     }
+    a {
+      color: $blue-color;
+      font-weight: normal;
+      text-decoration: underline $blue-color;
+    }
   }
   .collapsing {
-    @include font($uni,$regular,18px,24.3px,$secondary);
+    @include font($uni,$light,18px,24.3px,$base);
     @media screen and (max-width: 767px) {
       font-size: 14px;
+    }
+    a {
+      color: $blue-color;
+      font-weight: normal;
+      text-decoration: underline $blue-color;
     }
   }
 }

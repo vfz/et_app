397bc06dc49c9637c2e27fec483a374dbe2d70f7
Daniil Bobrov <bobdaniil@yandex.ru>
2022-11-09 17:07:17 +0300

    fix carousel-bootstrap on mobile for Ticket-booking.vue

--- src/views/Ticket-booking.vue
+++ src/views/Ticket-booking.vue
@@ -12,8 +12,8 @@
           <div class="col-12 p-0">
             <div id="carouselPath" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button v-if="!oneWay" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselPath" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button v-if="!oneWay" type="button" data-bs-target="#carouselPath" data-bs-slide-to="1" aria-label="Slide 2"></button>
               </div>
               <div class="carousel-inner">
                 <div class="carousel-item active">

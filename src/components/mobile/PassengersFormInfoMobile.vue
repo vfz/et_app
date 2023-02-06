68190c934e0401229913824f2ceb7084e858f3de
Daniil Bobrov <bobdaniil@yandex.ru>
2023-02-06 12:07:32 +0300

    format file

--- src/components/mobile/PassengersFormInfoMobile.vue
+++ src/components/mobile/PassengersFormInfoMobile.vue
@@ -14,10 +14,10 @@
       <div class="row">
         <div class="col-12 col-lg-8 col-xl-9">
           <form class="needs-validation" novalidate @keydown.enter.prevent>
-            <PassengersInfoMobile/>
-            <BuyerInfo/>
-            <TicketInfo/>
-            <BaggageRules/>
+            <PassengersInfoMobile />
+            <BuyerInfo />
+            <TicketInfo />
+            <BaggageRules />
           </form>
         </div>
       </div>
@@ -36,33 +36,39 @@ import PassengersInfoMobile from "@/components/mobile/PassengersInfoMobile";
 
 export default {
   name: "PassengersFormInfoMobile",
-  components: {TicketInfo, BaggageRules, BuyerInfo, PassengersInfoMobile, CheckIcon, ArrowPathIcon, CancelIcon},
+  components: { TicketInfo, BaggageRules, BuyerInfo, PassengersInfoMobile, CheckIcon, ArrowPathIcon, CancelIcon },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "src/assets/variables.scss";
 @import "src/assets/font.scss";
+
 .passengers-form-info-section {
   margin-left: 88px;
   margin-right: 88px;
   margin-top: 40px;
+
   @media screen and (max-width: 767px) {
     margin-top: 24px;
     margin-left: 8px;
     margin-right: 8px;
   }
+
   .title-section {
-    @include font($uni,$bold,24px,32.4px,$base);
+    @include font($uni, $bold, 24px, 32.4px, $base);
     margin-bottom: 16px;
+
     @media screen and (max-width: 767px) {
       font-size: 18px;
       line-height: 24.3px;
     }
   }
+
   .title-section-description {
-    @include font($uni,$light,18px,24.3px,$base);
+    @include font($uni, $light, 18px, 24.3px, $base);
     margin-bottom: 40px;
+
     @media screen and (max-width: 767px) {
       font-size: 14px;
       line-height: 18.9px;
